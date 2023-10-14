4b9de5b66786c3a0ecc6a0877288e60e0334c208
nareeeee <nare.unusyan@tumo.org>
2023-10-14 15:28:55 +0400

    new game

---
+++ fs.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+function main(){
+fs.writeFile("hello.txt", "Hello world\n", function(err){
+console.log("fs.writeFile ended");
+});
+console.log("fs.writeFile");
+}
+main();
---
+++ test.js
@@ -0,0 +1,25 @@
+// let p = document.getElementById("pElement")
+
+// p.addEventListener("click", poxianun)
+
+// function poxianun() {
+//     p.innerText = "ty"
+// }
+
+// function bodyClick(evt) {
+//     console.log("Clicked at X: " + evt.pageX + ", Y: " + evt.pageY);
+// }
+
+// window.onclick = bodyClick;
+
+// function setup(){
+//     createCanvas(500,500)
+// }
+
+// function ..
+
+function keydown(evt) {
+    console.log("You printed " + evt.key);
+    }
+   
+ window.onkeydown = keydown;
